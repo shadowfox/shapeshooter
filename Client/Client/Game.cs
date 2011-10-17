b0389c03ec100021154d47869c4d5b73e55632c6
Michael J <shadowfox19@gmail.com>
2011-10-17 23:51:44 +1000

    Background colour change

--- Client/Client/Game.cs
+++ Client/Client/Game.cs
@@ -141,7 +141,7 @@ namespace Client
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
 
